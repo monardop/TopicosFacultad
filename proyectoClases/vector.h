5f6e5056b25172dbaa14318446c2c8fbd1e9de3b
Pablo <pablomonardo@gmail.com>
2023-05-17 18:40:39 -0300

    Finalizo las firmas

--- proyectoClases/vector.h
+++ proyectoClases/vector.h
@@ -2,12 +2,13 @@
 #define VECTOR_H_INCLUDED
 
 void mapear(void* arr, int nroElementos, int tamElementos, int(*func)(void*));
-void filtrar(void* arr, int nroElementos, int tamElementos, int(*cmp)(void*, void*));
+void filtrar(void* arr, int nroElementos, int tamElementos, int(*cmp)(const void*, const void*));
 void reducir(void* arr, int nroElementos, int tamElementos, void(*func)(void*));
-int busquedaBinaria(void *arr, int nroElementos, int tamElementos, void* elem);
-void insertarOrdenado(void *arr, int nroElementos, int tamElementos,);
-void ordenarBurbujeo(void *arr, int nroElementos, int tamElementos,);
-void quickSort(void *arr, int nroElementos, int tamElementos,);
+int busquedaBinaria(void *arr, int nroElementos, int tamElementos, void* elem, void* max, void* min);
+void insertarOrdenado(void *arr, int nroElementos, int tamElementos);
+void ordenarBurbujeo(void *arr, int nroElementos, int tamElementos);
+void quickSort(void *arr, int nroElementos, int tamElementos, void* elem, void* max, void* min);
+
 
 
 #endif // VECTOR_H_INCLUDED
