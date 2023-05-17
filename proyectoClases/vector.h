a8a1d5c63d988ec7de841889d5359bb4003c4080
Pablo <pablomonardo@gmail.com>
2023-05-17 18:22:58 -0300

    Creo las firmas

--- proyectoClases/vector.h
+++ proyectoClases/vector.h
@@ -1,6 +1,13 @@
 #ifndef VECTOR_H_INCLUDED
 #define VECTOR_H_INCLUDED
 
+void mapear(void* arr, int nroElementos, int tamElementos, int(*func)(void*));
+void filtrar(void* arr, int nroElementos, int tamElementos, int(*cmp)(void*, void*));
+void reducir(void* arr, int nroElementos, int tamElementos, void(*func)(void*));
+int busquedaBinaria(void *arr, int nroElementos, int tamElementos, void* elem);
+void insertarOrdenado(void *arr, int nroElementos, int tamElementos,);
+void ordenarBurbujeo(void *arr, int nroElementos, int tamElementos,);
+void quickSort(void *arr, int nroElementos, int tamElementos,);
 
 
 #endif // VECTOR_H_INCLUDED
