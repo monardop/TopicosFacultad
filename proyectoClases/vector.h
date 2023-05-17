e6f066f4da573bcd1d6a4bf53d08c3f3decad2bf
Pablo <pablomonardo@gmail.com>
2023-05-17 19:08:18 -0300

    Modifoco filtrar

--- proyectoClases/vector.h
+++ proyectoClases/vector.h
@@ -2,7 +2,7 @@
 #define VECTOR_H_INCLUDED
 
 void mapear(void* arr, int nroElementos, int tamElementos, int(*func)(void*));
-void filtrar(void* arr, int nroElementos, int tamElementos, int(*cmp)(const void*, const void*));
+void filtrar(void* arr, int nroElementos, int tamElementos,, void*, int(*cmp)(const void*, const void*));
 void reducir(void* arr, int nroElementos, int tamElementos, void(*func)(void*));
 int busquedaBinaria(void *arr, int nroElementos, int tamElementos, void* elem, void* max, void* min);
 void insertarOrdenado(void *arr, int nroElementos, int tamElementos);
