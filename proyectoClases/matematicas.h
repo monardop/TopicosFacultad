aba3a023859e78e037e20c493a19076536c1b9eb
Pablo <pablomonardo@gmail.com>
2023-05-19 13:50:47 -0300

    Create matematicas.h

---
+++ proyectoClases/matematicas.h
@@ -0,0 +1,16 @@
+#ifndef MATEMATICAS_H_INCLUDED
+#define MATEMATICAS_H_INCLUDED
+
+#define PI 3.141592
+#define EULER 2.71828
+
+double pow(double, int);
+unsigned int factorial(unsigned int);
+double seno(float);
+double coseno(float);
+double tan(float);
+double potenciaEuler(int);
+float calcularRaizCuadrada(float);
+double abs(double);
+
+#endif // MATEMATICAS_H_INCLUDED
