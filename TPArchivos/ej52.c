9d36b298849fa13b43734c9a125c26d6063b366a
Pablo <pablomonardo@gmail.com>
2023-06-12 14:53:20 -0300

    Update ej52.c

--- TPArchivos/ej52.c
+++ TPArchivos/ej52.c
@@ -1,72 +1,87 @@
 #include <stdio.h>
-
+#include <string.h>
 /*
-Recibo un archivo de palabras separadas por punto o el caracter nulo. El 
+Recibo un string de palabras separadas por punto o el caracter nulo. El
 objetivo es leer el archivo, revisar cuantas veces se repita la primera palabra,
 cuantas palabras hay y cuantos caracteres tiene la palabra más larga.
 _EN_
-I receive a file with words separated by a period or the null character. The 
-objective is to read the file, check how many times the first word is repeated, 
+I receive a string with words separated by a period or the null character. The
+objective is to read the file, check how many times the first word is repeated,
 count the total number of words, and determine the length of the longest word.
 */
 
-void fileReader(const char*, const char*);
+#define MAXLEN 100
+void lector(const char* string);
+void limpiarstr(char*);
 
 int main(){
-    fileReader("palabras.txt", "rt");
+    char string[MAXLEN] = "pablo.pablo.pablo.holasoypablo.pablo";
 
-    return 0;    
+    // printf("Inserte palabras separadas por un punto:\n");
+    // fgets(string, sizeof(string), stdin);
+    
+    limpiarstr(string);
+    printf("%s\n", string);
+    lector(string);
+
+    return 0;
 }
 
+void limpiarstr(char* str){
+    int i=0;
+    while(*str && i < MAXLEN){
+        if(*str == '\n'){
+            *str = '\0';
+            break;
+        }
+        printf("%c - %d\t", *str, i);
+        str++;
+        i++;
+        printf("\n");
+    }
+    if(i == 99){
+        *str = '\0';
+    }
+}
 
-int stringLenght(const char* string){
-    int count = 0;
-
-    while(*string != '\0' && *string != '.'){
-        count++;
+int buscarFinPalabra(char* string){
+    int contador = 1;
+    while(*string){
+        if(*string == '.'){
+            break;
+        }
+        contador++;
         string++;
-    }   
-
-    return count;
-}
-
-int stringCompare(const char* str1, const char* str2){
-    int counter = 0;
-    while(*str1 && *str1 != '.'){
-        counter += (int)(*str1);
-        str1++;
     }
-    while(*str2 && *str1 != '.'){
-        counter -= (int)(*str2);
-        str2++;
-    }
-    return counter;
+
+    return contador;
 }
 
-void resultPrinter(char* firstWord, const int words, const int maxlen, const int firstTimes){
-    printf("The first word is: %s and it appeared %d times.\n", *firstWord, firstTimes);
-    printf("%d words in this file. \n", words);
-    printf("The length of the longest word was %d chars.\n", maxlen);
-}
+void lector(const char* string){
+    int contadorPalabras = 1, maxLongitud, repeticionPrimera = 0;
+    char primeraPalabra[MAXLEN];
+    char* punteroPalabras = string;
+    printf("%p\n", punteroPalabras);
+    maxLongitud = buscarFinPalabra(punteroPalabras);
+    strncpy(primeraPalabra, string, maxLongitud-1);
+    punteroPalabras += (maxLongitud-1);
 
-void fileReader(const char* path, const char* mode){
-    FILE* words = fopen(path, mode);
-    char newWord[30], firstWord[30];
-    unsigned maxLen, wordsCounter = 1, firstWordTimes = 0;
-
-    fgets(firstWord, 30, words);
-    maxLen = stringLenght(firstWord);
-    printf("%s", firstWord);
-    while (fgets(newWord,30,words) != NULL){
-        int newLen = stringLenght(newWord);
-        if(newLen > maxLen){
-            maxLen = newLen;
+    while(*punteroPalabras){
+        printf("%p  ", punteroPalabras);
+        int nuevaPalabra = buscarFinPalabra(punteroPalabras);
+        if(nuevaPalabra > maxLongitud){
+            maxLongitud = nuevaPalabra;
         }
-        if(stringCompare(firstWord, newWord) == 0){
-            firstWordTimes++;
+
+        if(strncmp(primeraPalabra, punteroPalabras, nuevaPalabra) == 0){
+            repeticionPrimera++;
         }
-        wordsCounter++;
+        contadorPalabras++;
+        punteroPalabras += nuevaPalabra;
     }
-    fclose(words); 
-    // resultPrinter(firstWord,wordsCounter,maxLen,firstWordTimes);
+
+    printf("\n");
+    printf("-Primer palabra: %s\n-Cant palabras: %d\n-Repeticion primer palabra: %d", primeraPalabra, contadorPalabras, repeticionPrimera);
+    printf("\n-La long maxima es de %d", maxLongitud);
+    printf("\n");
 }
