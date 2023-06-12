171d1ca78aba7705af5bb2f331364d3bbc5e0c05
Pablo <pablomonardo@gmail.com>
2023-06-12 18:09:41 -0300

    Actualización

--- TPArchivos/ej57.c
+++ TPArchivos/ej57.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+int integridadArchivos();
+
+
 int main(){
-    
+
+}
+
+int integridadArchivos(){
+    if(fopen("empleados_promedio.txt", "r") == NULL || fopen("empleados_sueldo.txt", "r") == NULL){
+        return 0;
+    }
+    return 1;
 }
